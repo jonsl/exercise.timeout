e4535d56419e1da7336470b1eaf9657bf3f25f5a
Jon Slater <jon.slater@yamil.com>
2018-05-30 11:24:03 +0100

    core logic working; todo: refine output

--- src/main/Java/com/exercise/timeout/MemberVenueApp.java
+++ src/main/Java/com/exercise/timeout/MemberVenueApp.java
@@ -1,7 +1,12 @@
 package com.exercise.timeout;
 
 import com.exercise.timeout.api.Member;
+import com.exercise.timeout.api.Venue;
 import com.exercise.timeout.model.JsonFeeds;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MemberVenueApp {
 
@@ -10,6 +15,11 @@ public class MemberVenueApp {
         try {
             JsonFeeds jsonFeeds = new JsonFeeds();
 
+            // start with everything in places to go
+            // and remove depending on who cant eat
+            ArrayList<Venue> placesToGo = new ArrayList<Venue>(jsonFeeds.getVenueList());
+            Map<String/*venue name*/, Member> avoidMap = new HashMap<String, Member>();
+            ArrayList<Venue> placesToAvoid = new ArrayList<Venue>();
 
             for (String name : args) {
 
@@ -18,8 +28,35 @@ public class MemberVenueApp {
 
                 if (member != null) {
 
-                }
+                    for (String wontEatItem : member.getWontEat()) {
 
+                        for (Venue venue : jsonFeeds.getVenueList()) {
+
+                            ArrayList<Member> membersWhoCantEat = new ArrayList<Member>();
+                            boolean canEat = false;
+
+                            for (String food : venue.getFood()) {
+                                if (!food.equals(wontEatItem)) {
+                                    canEat = true;
+                                }
+                            }
+
+                            if (!canEat) {
+                                placesToAvoid.add(venue);
+                                placesToGo.remove(venue);
+                            }
+                        }
+                    }
+                }
+            }
+
+            System.out.println("Places to go:");
+            for (Venue venue : placesToGo) {
+                System.out.println("\t" + venue.getName());
+            }
+            System.out.println("Places to avoid:");
+            for (Venue venue : placesToAvoid) {
+                System.out.println("\t" + venue.getName());
             }
 
         } catch (Exception e) {
