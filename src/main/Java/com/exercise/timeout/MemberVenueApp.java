a062cbfc41262aaeafb90ffd668043fdc0579728
Jon Slater <jon.slater@yamil.com>
2018-05-30 12:33:40 +0100

    fixed bug; fixed added names; refactor

--- src/main/Java/com/exercise/timeout/MemberVenueApp.java
+++ src/main/Java/com/exercise/timeout/MemberVenueApp.java
@@ -4,9 +4,7 @@ import com.exercise.timeout.api.Member;
 import com.exercise.timeout.api.Venue;
 import com.exercise.timeout.model.JsonFeeds;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MemberVenueApp {
 
@@ -17,9 +15,10 @@ public class MemberVenueApp {
 
             // start with everything in places to go
             // and remove depending on who cant eat
-            ArrayList<Venue> placesToGo = new ArrayList<Venue>(jsonFeeds.getVenueList());
-            Map<String/*venue name*/, Member> avoidMap = new HashMap<String, Member>();
-            ArrayList<Venue> placesToAvoid = new ArrayList<Venue>();
+            Set<Venue> goVenueSet = new HashSet<Venue>(jsonFeeds.getVenueList());
+            Set<Venue> avoidVenueSet = new HashSet<Venue>();
+
+            Map<String/*venue name*/, Set<String/*member names*/>> avoidMap = new HashMap<String, Set<String>>();
 
             for (String name : args) {
 
@@ -29,10 +28,8 @@ public class MemberVenueApp {
                 if (member != null) {
 
                     for (String wontEatItem : member.getWontEat()) {
-
                         for (Venue venue : jsonFeeds.getVenueList()) {
 
-                            ArrayList<Member> membersWhoCantEat = new ArrayList<Member>();
                             boolean canEat = false;
 
                             for (String food : venue.getFood()) {
@@ -42,8 +39,34 @@ public class MemberVenueApp {
                             }
 
                             if (!canEat) {
-                                placesToAvoid.add(venue);
-                                placesToGo.remove(venue);
+                                // we cant go here
+                                avoidVenueSet.add(venue);
+                                Set<String> values = avoidMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
+                                values.add(name);
+
+                                goVenueSet.remove(venue);
+                            }
+                        }
+                    }
+
+                    for (String willDrinkItem : member.getDrinks()) {
+                        for (Venue venue : jsonFeeds.getVenueList()) {
+
+                            boolean canDrink = false;
+
+                            for (String drink : venue.getDrinks()) {
+                                if (drink.equals(willDrinkItem)) {
+                                    canDrink = true;
+                                }
+                            }
+
+                            if (!canDrink) {
+                                // we cant go here
+                                avoidVenueSet.add(venue);
+                                Set<String> values = avoidMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
+                                values.add(name);
+
+                                goVenueSet.remove(venue);
                             }
                         }
                     }
@@ -51,12 +74,15 @@ public class MemberVenueApp {
             }
 
             System.out.println("Places to go:");
-            for (Venue venue : placesToGo) {
+            for (Venue venue : goVenueSet) {
                 System.out.println("\t" + venue.getName());
             }
             System.out.println("Places to avoid:");
-            for (Venue venue : placesToAvoid) {
+            for (Venue venue : avoidVenueSet) {
                 System.out.println("\t" + venue.getName());
+                for (String name : avoidMap.get(venue.getName())) {
+                    System.out.println("\t\tThere is nothing for " + name);
+                }
             }
 
         } catch (Exception e) {
