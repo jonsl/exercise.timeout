3feec68173431e9123acf54d0009fb90f9dd0ae4
Jon Slater <jon.slater@yamil.com>
2018-05-30 12:54:53 +0100

    added distinction between eat and drink

--- src/main/Java/com/exercise/timeout/MemberVenueApp.java
+++ src/main/Java/com/exercise/timeout/MemberVenueApp.java
@@ -18,7 +18,8 @@ public class MemberVenueApp {
             Set<Venue> goVenueSet = new HashSet<Venue>(jsonFeeds.getVenueList());
             Set<Venue> avoidVenueSet = new HashSet<Venue>();
 
-            Map<String/*venue name*/, Set<String/*member names*/>> avoidMap = new HashMap<String, Set<String>>();
+            Map<String/*venue name*/, Set<String/*member names*/>> avoidEatMap = new HashMap<String, Set<String>>();
+            Map<String/*venue name*/, Set<String/*member names*/>> avoidDrinkMap = new HashMap<String, Set<String>>();
 
             for (String name : args) {
 
@@ -41,7 +42,7 @@ public class MemberVenueApp {
                             if (!canEat) {
                                 // we cant go here
                                 avoidVenueSet.add(venue);
-                                Set<String> values = avoidMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
+                                Set<String> values = avoidEatMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
                                 values.add(name);
 
                                 goVenueSet.remove(venue);
@@ -63,7 +64,7 @@ public class MemberVenueApp {
                             if (!canDrink) {
                                 // we cant go here
                                 avoidVenueSet.add(venue);
-                                Set<String> values = avoidMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
+                                Set<String> values = avoidDrinkMap.computeIfAbsent(venue.getName(), k -> new HashSet<String>());
                                 values.add(name);
 
                                 goVenueSet.remove(venue);
@@ -80,8 +81,15 @@ public class MemberVenueApp {
             System.out.println("Places to avoid:");
             for (Venue venue : avoidVenueSet) {
                 System.out.println("\t" + venue.getName());
-                for (String name : avoidMap.get(venue.getName())) {
-                    System.out.println("\t\tThere is nothing for " + name);
+                if (avoidEatMap.get(venue.getName()) != null) {
+                    for (String name : avoidEatMap.get(venue.getName())) {
+                        System.out.println("\t\tThere is nothing for " + name + " to eat");
+                    }
+                }
+                if (avoidDrinkMap.get(venue.getName()) != null) {
+                    for (String name : avoidDrinkMap.get(venue.getName())) {
+                        System.out.println("\t\tThere is nothing for " + name + " to drink");
+                    }
                 }
             }
 
